e9dadfb4e1d869e0644e3b4ff7f68ecc22dc7c47
Julian <julian.lara@unillanos.edu.co>
2024-02-18 12:32:55 -0500

    Se crea el endpoint con los HTTP permitidos
    se agrega coleccion exportada de postman

--- tareas/urls_api.py
+++ tareas/urls_api.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
+from tareas.viewsets import TareasViewset
 
 router = DefaultRouter()
-
+router.register('tareas', TareasViewset)
 
 
 urlpatterns = [
--- tareas/viewsets.py
+++ tareas/viewsets.py
@@ -1,5 +1,6 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from tareas.models import Tareas
 from tareas.serializers import TareasSerializer
@@ -14,3 +15,54 @@ class TareasViewset(viewsets.ModelViewSet):
     ordering = ('pk',)
     ordering_fields = ('pk',)
 
+    def list(self, request, *args, **kwargs):
+        try:
+            queryset = self.get_queryset()
+            serializer = self.get_serializer(
+                queryset,
+                many=True,
+                context={
+                    'request': request,
+                }
+            )
+            return Response(data=serializer.data, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    def retrieve(self, request, *args, **kwargs):
+        try:
+            tarea = self.get_object()
+            return Response(self.serializer_class(tarea).data, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    def create(self, request, *args, **kwargs):
+        try:
+            serializer = TareasSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(data=serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    def partial_update(self, request, *args, **kwargs):
+        try:
+            tarea = self.get_object()
+            serializer = self.get_serializer(tarea, data=request.data, partial=True)
+            if serializer.is_valid():
+                tarea.estado = serializer.validated_data['estado']
+                tarea.save()
+                # serializer.save()
+                return Response(data=serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({'error': 'Unicamente debe actualizarse el campo estado'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    def destroy(self, request, *args, **kwargs):
+        try:
+            tarea = self.get_object()
+            tarea.delete()
+            return Response('Tarea eliminada Correctamente')
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
