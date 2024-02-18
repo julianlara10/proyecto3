f26726f79753e79cfbfa6408a9a077922b99bb67
Julian <julian.lara@unillanos.edu.co>
2024-02-18 13:45:46 -0500

    Creacion de pruebas unitarias sencillas para los endpoint

--- tareas/tests.py
+++ tareas/tests.py
@@ -1,3 +1,37 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
+from .models import Tareas
 
-# Create your tests here.
+
+class TareasViewSetTestCase(APITestCase):
+
+    def setUp(self):
+        self.Tareas = Tareas.objects.create(titulo='Tarea uno', descripcion='Esta es la tarea uno')
+        self.url = reverse('Tareas-list')
+
+    def test_listar_Tareas(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_obtener_un_Tareas(self):
+        url_un_Tareas = reverse('Tareas-detail', args=[self.Tareas.id])
+        response = self.client.get(url_un_Tareas)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_crear_un_Tareas(self):
+        data = {'titulo': 'Tarea dos', 'descripcion': 'Esta es la tarea dos'}
+        response = self.client.post(self.url, data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_actualizar_un_Tareas(self):
+        url_un_Tareas = reverse('Tareas-detail', args=[self.Tareas.id])
+        data = {'estado': 'en_progreso'}
+        response = self.client.put(url_un_Tareas, data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_eliminar_un_Tareas(self):
+        url_un_Tareas = reverse('Tareas-detail', args=[self.Tareas.id])
+        response = self.client.delete(url_un_Tareas)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
